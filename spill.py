71fd29f2d5a1069d904b5cd4c5e0992ead89820a
petterwt <tverdalp@outlook.com>
2022-12-15 10:14:58 +0100

    usikker
    ∂awi udbhwubdiu

--- spill.py
+++ spill.py
@@ -1,56 +1,237 @@
+import time
+import random
+print(u"{}[2J{}[;H".format(chr(27), chr(27)))
+
+
 class Spiller:
-    def __init__(self, navn, balanse, ):
+    def __init__(self, navn, balanse):
         self.navn = navn
+        self.balanse(balanse)
+
+    def balanse(self, balanse):
+        self.new_method(balanse)
+
+    def new_method(self, balanse):
         self.balanse = balanse
 
     def visInfo(self):
-        return print(f"\nNavnet på spilleren er {self.navn}, og har {self.balanse} kroner")
+        return print(f"\n Hei {self.navn}. Du har {self.balanse} kroner.")
 
 
-class Spill:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+spillernavn = input(str("Velg et navn: "))
+
+spiller = Spiller(spillernavn, 1000)
+
+print(spiller)
+
+time.sleep(1)
+
+spiller_balanse = spiller.balanse
+
+playing = False
 
 
-spiller = Spiller(input(str("Skriv navn: ")), 1000)
+suits = ("Spades ♠", "Clubs ♣", "Hearts ♥", "Diamonds ♦")
+ranks = ("1", "2", "3", "4", "5", "6", "7",
+         "8", "9", "10", "J", "Q", "K", "A",)
+values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
+          "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11, }
+# CLASS DEFINTIONS:
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+
+class Deck:
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = ""  # start with an empty string
+        for card in self.deck:
+            deck_comp += "\n " + card.__str__()  # add each Card object's print string
+        return "The deck has:" + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0  # start with zero value
+        self.aces = 0  # add an attribute to keep track of aces
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == "A":
+            self.aces += 1  # add to self.aces
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+# FUNCTION DEFINITIONS:
+
+
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
+
+
+def hit_or_stand(deck, hand):
+    global playing
+    while True:
+        x = input("\nWould you like to Hit or Stand? Enter [h/s] ")
+        if x[0].lower() == "h":
+            hit(deck, hand)  # hit() function defined above
+        elif x[0].lower() == "s":
+            print("Player stands. Dealer is playing.")
+            playing = False
+        else:
+            print("Sorry, Invalid Input. Please enter [h/s].")
+            continue
+        break
+
+
+def show_some(player, dealer):
+    print("\nPlayer's Hand:", *player.cards, sep="\n ")
+    print("Player's Hand =", player.value)
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print("", dealer.cards[1])
+
+
+def show_all(player, dealer):
+    print("\nPlayer's Hand:", *player.cards, sep="\n ")
+    print("Player's Hand =", player.value)
+    print("\nDealer's Hand:", *dealer.cards, sep="\n ")
+    print("Dealer's Hand =", dealer.value)
+
+
+def player_busts(player, dealer):
+    print("\n--- Player busts! ---")
+
+
+def player_wins(player, dealer):
+    print("\n--- Player has blackjack! You win! ---")
+
+
+def dealer_busts(player, dealer):
+    print("\n--- Dealer busts! You win! ---")
+
+
+def dealer_wins(player, dealer):
+    print("\n--- Dealer wins! ---")
+
+
+def push(player, dealer):
+    print("\nIts a tie!")
 
 
 def main():
-    while spiller.balanse > 0:
+    while spiller_balanse > 0:
         action = input(
-            "\n Hva vil du gjøre? \n(Skriv 'hjelp' for å få en liste av ting du kan gjøre) \n: ")
+            "\n Hva vil du gjøre? \n(Skriv 'hjelp' eller 'h' for å få en liste av ting du kan gjøre) \n: ")
 
-        if action.lower() == "hjelp":
+        if action.lower() == "hjelp" or "h":
             print("\n Ting du kan gjøre:")
-            print("- G: Gå en annen plass.")
+            print("- BJ: Spill blackjack")
             print("- Q: Quit game\n")
 
-        if action.lower() == "Q":
-            print("Du har valgt å slutte å spille.")
+        if action.lower() == "bj" or "blackjack":
+            bet = input(
+                f"Hvor mye vil du legge inn? \n   Du har {spiller_balanse} kroner.")
+
+            # GAMEPLAY!
+
+            playing = True
+
+            while playing == True:
+                time.sleep(1)
+
+                print(
+                    "   Welcome to BlackJack! \n   Game Rules: Get as close to 21 as you can without going over!\n   Dealer hits until it reaches 17.\n   Aces count as 1 or 11, depending on how much you have.")
+
+                # Create & shuffle the deck, deal two cards to each player
+                deck = Deck()
+                deck.shuffle()
+
+                player_hand = Hand()
+                player_hand.add_card(deck.deal())
+                player_hand.add_card(deck.deal())
+
+                dealer_hand = Hand()
+                dealer_hand.add_card(deck.deal())
+                dealer_hand.add_card(deck.deal())
+
+                # Show the cards:
+                show_some(player_hand, dealer_hand)
+
+                while playing:  # recall this variable from our hit_or_stand function
+
+                    # Prompt for Player to Hit or Stand
+                    hit_or_stand(deck, player_hand)
+                    show_some(player_hand, dealer_hand)
+
+                    if player_hand.value > 21:
+                        player_busts(player_hand, dealer_hand)
+                        break
+
+                # If Player hasn't busted, play Dealer's hand
+                if player_hand.value <= 21:
+
+                    while dealer_hand.value < 17:
+                        hit(deck, dealer_hand)
+
+                    # Show all cards
+                    time.sleep(1)
+                    print("Results:")
+
+                    show_all(player_hand, dealer_hand)
+
+                    # Test different winning scenarios
+                    if dealer_hand.value > 21:
+                        dealer_busts(player_hand, dealer_hand)
+
+                    elif dealer_hand.value > player_hand.value:
+                        dealer_wins(player_hand, dealer_hand)
+
+                    elif dealer_hand.value < player_hand.value:
+                        player_wins(player_hand, dealer_hand)
+
+                    else:
+                        push(player_hand, dealer_hand)
+
+                # Ask to play again
+                new_game = input("\nPlay another hand? [Y/N] ")
+                while new_game.lower() not in ["y", "n"]:
+                    new_game = input("Invalid Input. Please enter 'y' or 'n' ")
+                if new_game[0].lower() == "y":
+                    playing = True
+                    continue
+                else:
+                    print(
+                        "\n     Thanks for playing      \n")
+                    break
 
 
 spiller.visInfo()
 
+time.sleep(1.5)
 
 main()
-
-
-# def main():
-#     player = Spiller("Spiller", 100)
-
-#     # Main game loop
-#     while player.balance > 0:
-#         # Prompt the player for an action
-#         action = input(
-#             "\nWhat would you like to do? \n(Skriv 'hjelp' for å få en liste av ting du kan gjøre. \n: ")
-
-#         # Process the player's action
-#         if action.lower() == "hjelp":
-#             print("\n Ting du kan gjøre:")
-#             print("- A: Gjett nummeret")
-#             print("- Q: Quit game\n")
-
-
-# main()
